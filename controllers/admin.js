4d0c2caeb3466df79e3530de1a9324c11d1d7c47
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-05-27 17:40:07 -0300

    Teste routes for admin

---
+++ controllers/admin.js
@@ -0,0 +1,6 @@
+//const recipes = require('../data')
+
+
+exports.index = function(req, res){
+    return res.render('admin/teste')
+}
--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const user = require('./controllers/user')
-
+const admin = require('./controllers/admin')
 const routes = express.Router()
 
 routes.get('/', user.index)
@@ -11,14 +11,7 @@ routes.use(function(req, res) {
     return res.status(404).render('./users/not-found')
 })
 
-//Admin routes ==== Not done yet ====
-// routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
-// routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
-// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+routes.get("/admin/teste", admin.index); 
 
-// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
-// routes.put("/admin/recipes", recipes.put); // Editar uma receita
-// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
 module.exports = routes
