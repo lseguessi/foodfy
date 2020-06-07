9d722e98501b87ea9d117d20e4e6d62e3106a2be
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-06-07 20:55:17 -0300

    Page create recipe

--- public/scripts_admin.js
+++ public/scripts_admin.js
@@ -19,3 +19,21 @@ for(let i=0; i<receita.length; i++){
 //     })    
 // }
 
+
+// Adicionar ingredientes
+function addIngrediente(){
+    const ingredients = document.querySelector('#ingredients')
+    const fieldContainer = document.querySelectorAll('.ingredients')
+
+    //Realizar um clone do útlimo ingrediente
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+    //Não adicionar um novo campo, caso o último esteja vazio
+    if(newField.children[0].value == "") return false
+
+    //Deixa valor do input vazio
+    newField.children[0].value = ""
+    ingredients.appendChild(newField)
+}
+
+document.querySelector('.add-ingredient').addEventListener('click', addIngrediente)
